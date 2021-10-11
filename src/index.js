c42001afaeef28798a28ad0bf401ce4b806b4377
hati8haha <haoting3462@gmail.com>
2021-10-11 20:55:35 +0800

    getProducts

--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import { Provider as StyletronProvider, DebugEngine } from "styletron-react";
-import { Client as Styletron } from "styletron-engine-atomic";
-
-const debug =
-  process.env.NODE_ENV === "production" ? void 0 : new DebugEngine();
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import { Provider as StyletronProvider, DebugEngine } from 'styletron-react'
+import { Client as Styletron } from 'styletron-engine-atomic'
+import { Provider } from 'react-redux'
+import store from './redux/store'
+const debug = process.env.NODE_ENV === 'production' ? void 0 : new DebugEngine()
 
 // 1. Create a client engine instance
-const engine = new Styletron();
+const engine = new Styletron()
 
 // 2. Provide the engine to the app
 // debug engine needs inlined source maps
 ReactDOM.render(
   <StyletronProvider value={engine} debug={debug} debugAfterHydration>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StyletronProvider>,
-  document.getElementById("root")
-);
+  document.getElementById('root')
+)
