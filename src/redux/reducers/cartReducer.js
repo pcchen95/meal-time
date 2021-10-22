46ffda74482f39d28b37a8bf8d66a5be2ea6dc72
hati8haha <haoting3462@gmail.com>
2021-10-22 11:06:59 +0800

    cart Reducer

--- src/redux/reducers/cartReducer.js
+++ src/redux/reducers/cartReducer.js
@@ -1,10 +1,9 @@
 import { createSlice } from "@reduxjs/toolkit"
-import { getProduct as getProductApi } from "../../WebAPI/productAPI"
+import { getCartData as getCartDataApi } from "../../WebAPI/cartAPI"
 import { setErrorMessage } from "./notificationReducer"
 
 const initialState = {
   cart: null,
-  cartData: null,
 }
 
 export const cartReducer = createSlice({
@@ -14,47 +13,35 @@ export const cartReducer = createSlice({
     setCart: (state, action) => {
       state.cart = action.payload
     },
-    setCartData: (state, action) => {
-      state.cartData = action.payload
-    },
   },
 })
 
-export const { setCart, setCartData } = cartReducer.actions
-
-export const getCartFromLocalStorage = (userId) => (dispatch) => {
-  const cartArray = JSON.parse(localStorage.getItem(`cartId${userId}`))
-  if (cartArray) dispatch(setCart(cartArray))
-}
-
-export const cleanCart = () => (dispatch) => {
-  dispatch(setCart(null))
-}
+export const { setCart } = cartReducer.actions
 
 export const getCartData = (userId) => (dispatch) => {
-  const cartArray = JSON.parse(localStorage.getItem(`cartId${userId}`))
-  const newCartData = []
-
-  try {
-    cartArray.forEach((cartByVendor) =>
-      cartByVendor.cartItems.forEach(async (product) => {
-        const res = await getProductApi(product.productId)
-        if (!res.ok) {
-          return dispatch(
-            setErrorMessage(res ? res.message : "something wrong")
-          )
-        }
-        let productData = res.data
-        productData = { ...productData, cartQuantity: product.quantity }
-        newCartData.push(productData)
-        // dispatch(setCartData(newCartData))
-        console.log(newCartData)
-      })
-    )
-  } catch (err) {
-    console.log(err)
+  const cart = localStorage.getItem(`cartId${userId}`)
+  let cartArray
+  if (cart) {
+    cartArray = JSON.parse(cart)
+    console.log(cartArray)
   }
-  return newCartData
+  return getCartDataApi({ cart: cartArray })
+    .then((res) => {
+      if (!res.ok) {
+        return dispatch(setErrorMessage(res ? res.message : "something wrong"))
+      }
+      return res.data
+    })
+    .then((cartData) => {
+      return dispatch(setCart(cartData))
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+}
+
+export const cleanCartData = () => (dispatch) => {
+  dispatch(setCart(null))
 }
 
 export default cartReducer.reducer
