3708e301e9cd0500f03782b809d4183634539f02
MiluLin42 <cdef4120@hotmail.com>
2021-10-25 21:28:58 +0800

    pushAllUsed

--- src/redux/reducers/userReducer.js
+++ src/redux/reducers/userReducer.js
@@ -1,4 +1,4 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 import {
   register as registerApi,
   login as loginApi,
@@ -6,36 +6,42 @@ import {
   updateProfile as updateProfileApi,
   updatePassword as updatePasswordApi,
   getAllProfiles as getAllProfilesApi,
-} from '../../WebAPI/userAPI';
-import { setAuthToken } from '../../utils';
+} from "../../WebAPI/userAPI";
+import { setAuthToken } from "../../utils";
+import {
+  setErrorMessage,
+  setShowSuccessNotification,
+  setShowWarningNotification,
+} from "./notificationReducer";
 
 const initialState = {
-  errMessage: null,
   user: null,
   users: null,
+  isLoading: false,
 };
 
 export const userReducer = createSlice({
-  name: 'user',
+  name: "user",
   initialState,
   reducers: {
-    setErrMessage: (state, action) => {
-      state.errMessage = action.payload;
-    },
     setUser: (state, action) => {
       state.user = action.payload;
     },
     setUsers: (state, action) => {
       state.users = action.payload;
     },
+    setIsLoading: (state, action) => {
+      state.isLoading = action.payload;
+    },
   },
 });
 
-export const { setErrMessage, setUser, setUsers } = userReducer.actions;
+export const { setUser, setUsers, setIsLoading } = userReducer.actions;
 
 export const register =
   ({ avatar, nickname, username, password, email, phone }) =>
   (dispatch) => {
+    dispatch(setIsLoading(true));
     return registerApi({
       avatar,
       nickname,
@@ -44,16 +50,21 @@ export const register =
       email,
       phone,
     }).then((res) => {
+      dispatch(setIsLoading(false));
       if (!res.ok) {
-        setAuthToken(null);
-        return dispatch(setErrMessage(res.message));
+        setAuthToken("");
+        dispatch(setErrorMessage(res.message));
+        dispatch(setShowWarningNotification(true));
+        return;
       }
       setAuthToken(res.token);
-
+      dispatch(setShowSuccessNotification(true, "註冊、登入成功！"));
       return getMeApi().then((res) => {
         if (!res.ok) {
-          setAuthToken(null);
-          return dispatch(setErrMessage(res.message));
+          setAuthToken("");
+          dispatch(setErrorMessage(res.message));
+          dispatch(setShowWarningNotification(true));
+          return;
         }
         dispatch(setUser(res.data));
         return res.data;
@@ -62,17 +73,23 @@ export const register =
   };
 
 export const login = (username, password) => (dispatch) => {
+  dispatch(setIsLoading(true));
   return loginApi(username, password).then((res) => {
+    dispatch(setIsLoading(false));
     if (!res.ok) {
-      setAuthToken(null);
-      return dispatch(setErrMessage(res.message));
+      setAuthToken("");
+      dispatch(setErrorMessage(res.message));
+      dispatch(setShowWarningNotification(true));
+      return;
     }
     setAuthToken(res.token);
-
+    dispatch(setShowSuccessNotification(true, "登入成功！"));
     return getMeApi().then((res) => {
       if (!res.ok) {
-        setAuthToken(null);
-        return dispatch(setErrMessage(res.message));
+        setAuthToken("");
+        dispatch(setErrorMessage(res.message));
+        dispatch(setShowWarningNotification(true));
+        return;
       }
       dispatch(setUser(res.data));
       return res.data;
@@ -81,28 +98,44 @@ export const login = (username, password) => (dispatch) => {
 };
 
 export const getMe = () => (dispatch) => {
+  dispatch(setIsLoading(true));
   return getMeApi().then((res) => {
     if (!res.ok) {
-      return dispatch(setErrMessage(res.message));
+      dispatch(setErrorMessage(res.message));
+      return;
     }
+    dispatch(setIsLoading(false));
     dispatch(setUser(res.data));
   });
 };
 
 export const updateProfile =
-  ({ avatar, nickname, username, password, email, phone }) =>
+  ({ avatar, nickname, email, phone, isDeleteAvatar }) =>
   (dispatch) => {
+    dispatch(setIsLoading(true));
     return updateProfileApi({
       avatar,
       nickname,
-      username,
-      password,
       email,
       phone,
+      isDeleteAvatar,
     }).then((res) => {
       if (!res.ok) {
-        dispatch(setErrMessage(res.message));
+        dispatch(setErrorMessage(res.message));
+        dispatch(setShowWarningNotification(true));
+        return;
       }
+      dispatch(setShowSuccessNotification(true));
+      getMeApi().then((res) => {
+        if (!res.ok) {
+          dispatch(setErrorMessage(res.message));
+          dispatch(setShowWarningNotification(true));
+          return;
+        }
+        dispatch(setIsLoading(false));
+        dispatch(setUser(res.data));
+        return res;
+      });
     });
   };
 
@@ -111,8 +144,12 @@ export const updatePassword =
     return updatePasswordApi(oldPassword, newPassword, confirmPassword).then(
       (res) => {
         if (!res.ok) {
-          dispatch(setErrMessage(res.message));
+          dispatch(setErrorMessage(res.message));
+          dispatch(setShowWarningNotification(true));
+          return res;
         }
+        dispatch(setShowSuccessNotification(true));
+        return res;
       }
     );
   };
@@ -122,7 +159,9 @@ export const getAllProfiles =
   (dispatch) => {
     return getAllProfilesApi({ page, limit, sort, order, role }).then((res) => {
       if (!res.ok) {
-        return dispatch(setErrMessage(res.message));
+        dispatch(setErrorMessage(res.message));
+        dispatch(setShowWarningNotification(true));
+        return;
       }
       dispatch(setUsers(res.data));
     });
