ed34fb590b1f873a01d44eadfdd712876e718ad6
hati8haha <haoting3462@gmail.com>
2021-10-27 13:02:36 +0800

    bug fix

--- src/Components/ProductSystem/UploadButton.js
+++ src/Components/ProductSystem/UploadButton.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Div, Label, Input, Icon } from 'atomize'
+import React from "react";
+import PropTypes from "prop-types";
+import { Div, Label, Input, Icon } from "atomize";
 
 export default function UploadButton({ fileInput, handleEvent }) {
   return (
     <Div
-      m={{ l: { xs: '0', md: '1rem' } }}
+      m={{ l: { xs: "0", md: "1rem" } }}
       d="flex"
-      align={{ xs: 'center', md: 'flex-start' }}
+      align={{ xs: "center", md: "flex-start" }}
       justify="center"
       flexDir="column"
     >
@@ -17,13 +17,13 @@ export default function UploadButton({ fileInput, handleEvent }) {
           accept=".png,.jpg,.jpeg"
           ref={fileInput}
           onChange={handleEvent}
-          style={{ display: 'none' }}
+          style={{ display: "none" }}
         />
         <Div
           bg="info700"
           shadow="1"
           hoverShadow="3"
-          p={{ x: '1rem' }}
+          p={{ x: "1rem" }}
           h="2.5rem"
           textSize="body"
           textWeight="500"
@@ -40,18 +40,18 @@ export default function UploadButton({ fileInput, handleEvent }) {
             name="CameraSolid"
             size="16px"
             color="white"
-            m={{ l: '1rem' }}
+            m={{ l: "1rem" }}
           />
         </Div>
       </Label>
-      <Div textSize="14px" textColor="gray700" m={{ t: '1rem' }}>
-        {'僅限上傳 .PNG .JPG .JPEG 格式檔案，尺寸 < 1 MB'}
+      <Div textSize="14px" textColor="gray700" m={{ t: "1rem" }}>
+        {"僅限上傳 .PNG .JPG .JPEG 格式檔案，尺寸 < 1 MB"}
       </Div>
     </Div>
-  )
+  );
 }
 
 UploadButton.propTypes = {
-  fileInput: PropTypes.string,
+  fileInput: PropTypes.object,
   handleEvent: PropTypes.func,
-}
+};
