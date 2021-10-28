4631ede30bf1b58712c8cce4c5d4d5c4df120f3a
rexrexchian <rexrexchian@gmail.com>
2021-10-28 15:58:28 +0800

    admin pages

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,7 @@
 import React from "react";
-import { Div, Input, Icon, Image, Button } from "atomize";
+import { Div, Input, Icon, Image, Button, SideDrawer, Text } from "atomize";
+import { useNavigate } from "react-router-dom";
+import PropTypes from "prop-types";
 
 const InputWithRightIcon = () => {
   return (
@@ -32,7 +34,7 @@ const InputWithRightIcon = () => {
 
 const CategoryButton = () => {
   return (
-    <Div d="flex">
+    <Div d="flex" flexWrap={{ xs: "wrap" }} justify={{ xs: "space-around" }}>
       <Button
         h="2.5rem"
         p={{ x: "1.25rem" }}
@@ -40,7 +42,7 @@ const CategoryButton = () => {
         bg="white"
         hoverBg="warning300"
         rounded="circle"
-        m={{ r: "1rem" }}
+        m={{ r: { xs: "0.25rem", md: "1rem" }, b: { xs: "0.25rem" } }}
         fontFamily="code"
         textColor="info700"
         border="1px solid"
@@ -55,7 +57,7 @@ const CategoryButton = () => {
         bg="white"
         hoverBg="warning300"
         rounded="circle"
-        m={{ r: "1rem" }}
+        m={{ r: { xs: "0.25rem", md: "1rem" }, b: { xs: "0.25rem" } }}
         fontFamily="code"
         textColor="info700"
         border="1px solid"
@@ -70,7 +72,7 @@ const CategoryButton = () => {
         bg="white"
         hoverBg="warning300"
         rounded="circle"
-        m={{ r: "1rem" }}
+        m={{ r: { xs: "0.25rem", md: "1rem" }, b: { xs: "0.25rem" } }}
         fontFamily="code"
         textColor="info700"
         border="1px solid"
@@ -85,7 +87,7 @@ const CategoryButton = () => {
         bg="white"
         hoverBg="warning300"
         rounded="circle"
-        m={{ r: "1rem" }}
+        m={{ r: { xs: "0.25rem", md: "1rem" }, b: { xs: "0.25rem" } }}
         fontFamily="code"
         textColor="info700"
         border="1px solid"
@@ -100,7 +102,7 @@ const CategoryButton = () => {
         bg="white"
         hoverBg="warning300"
         rounded="circle"
-        m={{ r: "1rem" }}
+        m={{ r: { xs: "0.25rem", md: "1rem" }, b: { xs: "0.25rem" } }}
         fontFamily="code"
         textColor="info700"
         border="1px solid"
@@ -115,7 +117,7 @@ const CategoryButton = () => {
         bg="white"
         hoverBg="warning300"
         rounded="circle"
-        m={{ r: "1rem" }}
+        m={{ r: { xs: "0.25rem", md: "1rem" }, b: { xs: "0.25rem" } }}
         fontFamily="code"
         textColor="info700"
         border="1px solid"
@@ -130,7 +132,7 @@ const CategoryButton = () => {
         bg="white"
         hoverBg="warning300"
         rounded="circle"
-        m={{ r: "1rem" }}
+        m={{ r: { xs: "0.25rem", md: "1rem" }, b: { xs: "0.25rem" } }}
         fontFamily="code"
         textColor="info700"
         border="1px solid"
@@ -143,6 +145,8 @@ const CategoryButton = () => {
 };
 
 const LikeButton = () => {
+  const navigate = useNavigate();
+
   return (
     <Button
       h="2.5rem"
@@ -150,6 +154,7 @@ const LikeButton = () => {
       bg="white"
       hoverBg="warning400"
       rounded="circle"
+      onClick={() => navigate("/cart")}
     >
       <Icon name="HeartSolid" size="20px" color="danger700" />
     </Button>
@@ -170,21 +175,111 @@ const NotiButton = () => {
   );
 };
 
-const MenuButton = () => {
+const SizeSideDrawer = ({ isOpen, onClose }) => {
+  const navigate = useNavigate();
+
   return (
-    <Button
-      h="2.5rem"
-      w="2.5rem"
-      bg="white"
-      hoverBg="warning400"
-      rounded="circle"
+    <SideDrawer
+      isOpen={isOpen}
+      onClose={onClose}
+      w={{ xs: "100vw", sm: "16rem" }}
     >
-      <Icon name="Menu" size="20px" color="black700" />
-    </Button>
+      <Div m={{ b: "4rem" }} textAlign="center">
+        <Text
+          p={{ t: "2rem" }}
+          textSize="title"
+          textColor="black700"
+          hoverTextColor="info700"
+          cursor="pointer"
+          onClick={() => navigate("/member_edit")}
+        >
+          編輯個人資料
+        </Text>
+        <Text
+          p={{ t: "2rem" }}
+          textSize="title"
+          textColor="black700"
+          hoverTextColor="info700"
+          cursor="pointer"
+        >
+          申請成為賣家
+        </Text>{" "}
+        <Text
+          p={{ t: "2rem" }}
+          textSize="title"
+          textColor="black700"
+          hoverTextColor="info700"
+          cursor="pointer"
+          onClick={() => navigate("/update_store")}
+        >
+          管理商家頁面
+        </Text>{" "}
+        <Text
+          p={{ t: "2rem" }}
+          textSize="title"
+          textColor="black700"
+          hoverTextColor="info700"
+          cursor="pointer"
+        >
+          管理商品列表
+        </Text>
+        <Text
+          p={{ t: "2rem" }}
+          textSize="title"
+          textColor="black700"
+          hoverTextColor="info700"
+          cursor="pointer"
+          onClick={() => navigate("/orders")}
+        >
+          訂單詳情
+        </Text>
+      </Div>
+    </SideDrawer>
   );
 };
 
+class Drawer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showSideDrawer: false,
+    };
+  }
+  render() {
+    const { showSideDrawer } = this.state;
+    return (
+      <>
+        <Button
+          h="2.5rem"
+          w="2.5rem"
+          bg="white"
+          hoverBg="warning400"
+          rounded="circle"
+          onClick={() =>
+            this.setState({
+              showSideDrawer: true,
+            })
+          }
+        >
+          <Icon name="Menu" size="20px" color="black700" />
+        </Button>
+        <SizeSideDrawer
+          isOpen={showSideDrawer}
+          onClose={() => this.setState({ showSideDrawer: false })}
+        />
+      </>
+    );
+  }
+}
+
+SizeSideDrawer.propTypes = {
+  isOpen: PropTypes.bool,
+  onClose: PropTypes.func,
+};
+
 const UserButton = () => {
+  const navigate = useNavigate();
+
   return (
     <Button
       h="2.5rem"
@@ -193,6 +288,7 @@ const UserButton = () => {
       hoverBg="warning600"
       rounded="circle"
       m={{ l: "1rem" }}
+      onClick={() => navigate("/entrance")}
     >
       <Icon name="UserCircle" size="30px" color="black900" />
     </Button>
@@ -204,7 +300,7 @@ const NavBar = () => {
     <Div
       m={{ t: "1.5rem", l: "1.5rem", r: "1.5rem", b: "3rem" }}
       p={{ t: "2rem", l: "2rem", r: "2rem" }}
-      d="flex"
+      d={{ md: "flex" }}
       justify="space-between"
       border={{ b: "1px solid" }}
       borderColor="info400"
@@ -212,10 +308,10 @@ const NavBar = () => {
     >
       <InputWithRightIcon></InputWithRightIcon>
       <CategoryButton></CategoryButton>
-      <Div d="flex">
+      <Div d="flex" justify={{ xs: "center" }} m={{ t: { xs: "1rem" } }}>
         <LikeButton></LikeButton>
         <NotiButton></NotiButton>
-        <MenuButton></MenuButton>
+        <Drawer />
         <UserButton></UserButton>
       </Div>
     </Div>
--- src/webAPI/productAPI.js
+++ src/webAPI/productAPI.js
@@ -1,45 +1,45 @@
-import { BASE_URL } from '../constants/unit'
-import { getAuthToken } from '../utils'
+import BASE_URL from "../constants/apiurl";
+import { getAuthToken } from "../utils";
 
 const getQueryString = (queryParameters) => {
-  const { page, sort, order, limit } = queryParameters
-  let queryString = ''
-  if (page) queryString += `&page=${page}`
-  if (sort) queryString += `&sort=${sort}`
-  if (order) queryString += `&order=${order}`
-  if (limit) queryString += `&limit=${limit}`
-  return queryString
-}
+  const { page, sort, order, limit } = queryParameters;
+  let queryString = "";
+  if (page) queryString += `&page=${page}`;
+  if (sort) queryString += `&sort=${sort}`;
+  if (order) queryString += `&order=${order}`;
+  if (limit) queryString += `&limit=${limit}`;
+  return queryString;
+};
 
 const getProducts = (queryParameters) => {
-  const queryString = getQueryString(queryParameters)
-  return fetch(`${BASE_URL}/products?${queryString}`).then((res) => res.json())
-}
+  const queryString = getQueryString(queryParameters);
+  return fetch(`${BASE_URL}/products?${queryString}`).then((res) => res.json());
+};
 
 const getProductsByCategory = (id, queryParameters) => {
-  const queryString = getQueryString(queryParameters)
+  const queryString = getQueryString(queryParameters);
   return fetch(`${BASE_URL}/products/category/${id}?${queryString}`).then(
     (res) => res.json()
-  )
-}
+  );
+};
 
 const getProductsByVendor = (id, queryParameters) => {
-  const queryString = getQueryString(queryParameters)
+  const queryString = getQueryString(queryParameters);
   return fetch(`${BASE_URL}/products/vendor/${id}?${queryString}`).then((res) =>
     res.json()
-  )
-}
+  );
+};
 
 const searchProduct = (keyword, queryParameters) => {
-  const queryString = getQueryString(queryParameters)
+  const queryString = getQueryString(queryParameters);
   return fetch(
     `${BASE_URL}/products/search?_keyword=${keyword}${queryString}`
-  ).then((res) => res.json())
-}
+  ).then((res) => res.json());
+};
 
 const getProduct = (id) => {
-  return fetch(`${BASE_URL}/products/${id}`).then((res) => res.json())
-}
+  return fetch(`${BASE_URL}/products/${id}`).then((res) => res.json());
+};
 
 const postProduct = (data) => {
   const {
@@ -52,27 +52,27 @@ const postProduct = (data) => {
     description,
     isAvailable,
     picture,
-  } = data
-  const token = getAuthToken()
-  const formData = new FormData()
-  formData.append('name', name)
-  formData.append('categoryId', categoryId)
-  formData.append('price', price)
-  formData.append('quantity', quantity)
-  formData.append('manufactureDate', manufactureDate)
-  formData.append('expiryDate', expiryDate)
-  formData.append('description', description)
-  formData.append('isAvailable', isAvailable)
-  formData.append('picture', picture)
+  } = data;
+  const token = getAuthToken();
+  const formData = new FormData();
+  formData.append("name", name);
+  formData.append("categoryId", categoryId);
+  formData.append("price", price);
+  formData.append("quantity", quantity);
+  formData.append("manufactureDate", manufactureDate);
+  formData.append("expiryDate", expiryDate);
+  formData.append("description", description);
+  formData.append("isAvailable", isAvailable);
+  formData.append("picture", picture);
   return fetch(`${BASE_URL}/products`, {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'content-type': 'multipart/form-data',
+      "content-type": "multipart/form-data",
       authorization: `Bearer ${token}`,
     },
     body: formData,
-  }).then((res) => res.json())
-}
+  }).then((res) => res.json());
+};
 
 const updateProduct = (id, updatedData) => {
   const {
@@ -85,77 +85,77 @@ const updateProduct = (id, updatedData) => {
     description,
     isAvailable,
     picture,
-  } = updatedData
-  const token = getAuthToken()
-  const formData = new FormData()
-  formData.append('name', name)
-  formData.append('categoryId', categoryId)
-  formData.append('price', price)
-  formData.append('quantity', quantity)
-  formData.append('manufactureDate', manufactureDate)
-  formData.append('expiryDate', expiryDate)
-  formData.append('description', description)
-  formData.append('isAvailable', isAvailable)
-  formData.append('picture', picture)
+  } = updatedData;
+  const token = getAuthToken();
+  const formData = new FormData();
+  formData.append("name", name);
+  formData.append("categoryId", categoryId);
+  formData.append("price", price);
+  formData.append("quantity", quantity);
+  formData.append("manufactureDate", manufactureDate);
+  formData.append("expiryDate", expiryDate);
+  formData.append("description", description);
+  formData.append("isAvailable", isAvailable);
+  formData.append("picture", picture);
   return fetch(`${BASE_URL}/products/${id}`, {
-    method: 'PATCH',
+    method: "PATCH",
     headers: {
-      'content-type': 'multipart/form-data',
+      "content-type": "multipart/form-data",
       authorization: `Bearer ${token}`,
     },
     body: formData,
-  }).then((res) => res.json())
-}
+  }).then((res) => res.json());
+};
 
 const deleteProduct = (id) => {
-  const token = getAuthToken()
+  const token = getAuthToken();
   return fetch(`${BASE_URL}/products/${id}`, {
-    method: 'DELETE',
+    method: "DELETE",
     headers: {
-      'content-type': 'application/json',
+      "content-type": "application/json",
       authorization: `Bearer ${token}`,
     },
-  }).then((res) => res.json())
-}
+  }).then((res) => res.json());
+};
 
 const getProductCategories = () => {
-  return fetch(`${BASE_URL}/product-categories`).then((res) => res.json())
-}
+  return fetch(`${BASE_URL}/product-categories`).then((res) => res.json());
+};
 
 const addProductCategory = (name) => {
-  const token = getAuthToken()
+  const token = getAuthToken();
   return fetch(`${BASE_URL}/product-categories`, {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'content-type': 'application/json',
+      "content-type": "application/json",
       authorization: `Bearer ${token}`,
     },
     body: JSON.stringify({ name }),
-  }).then((res) => res.json())
-}
+  }).then((res) => res.json());
+};
 
 const updateProductCategory = (id, name) => {
-  const token = getAuthToken()
+  const token = getAuthToken();
   return fetch(`${BASE_URL}/product-categories/${id}`, {
-    method: 'PATCH',
+    method: "PATCH",
     headers: {
-      'content-type': 'application/json',
+      "content-type": "application/json",
       authorization: `Bearer ${token}`,
     },
     body: JSON.stringify({ name }),
-  }).then((res) => res.json())
-}
+  }).then((res) => res.json());
+};
 
 const deleteProductCategory = (id) => {
-  const token = getAuthToken()
+  const token = getAuthToken();
   return fetch(`${BASE_URL}/product-categories/${id}`, {
-    method: 'DELETE',
+    method: "DELETE",
     headers: {
-      'content-type': 'application/json',
+      "content-type": "application/json",
       authorization: `Bearer ${token}`,
     },
-  }).then((res) => res.json())
-}
+  }).then((res) => res.json());
+};
 
 export {
   getProducts,
@@ -170,4 +170,4 @@ export {
   addProductCategory,
   updateProductCategory,
   deleteProductCategory,
-}
+};
