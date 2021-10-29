01d55caba4fd24fbd8c96f2854d597dffa97a873
rexrexchian <rexrexchian@gmail.com>
2021-10-29 08:00:56 +0800

    admin pages

--- src/pages/AdminMemberPage/AdminMemberPage.js
+++ src/pages/AdminMemberPage/AdminMemberPage.js
@@ -1,11 +1,23 @@
 import React, { useState, useEffect } from "react";
 import { Div } from "atomize";
+//import { useSelector, useDispatch } from "react-redux";
+//import { getAllProfiles as getAllProfilesAPI } from "../../redux/reducers/adminReducer";
 import { login } from "../../WebAPI/userAPI";
-import { getAllProfiles, updateUserAuth } from "../../WebAPI/userAPI";
+import { updateUserAuth, getAllProfiles } from "../../WebAPI/userAPI";
+
+//import { getAllProfiles, updateUserAuth } from "../../WebAPI/userAPI";
 import { MemberFilterButton } from "../../Components/AdminSystem/FilterButton";
 import MemberList from "../../Components/AdminSystem/MemberList";
 
 const AdminMemberPage = () => {
+  /*
+  const dispatch = useDispatch();
+  const users = useSelector((store) => store.admin.users);
+  let usersData = Object.values(users)[1];
+  dispatch(getAllProfilesAPI({ page: 1 }));
+  const state = useSelector(selectAllUsers);
+  console.log(state.rows);
+  */
   const [users, setUsers] = useState([]);
   const [display, setDisplay] = useState("all");
   const [errorMessage, setErrorMessage] = useState();
@@ -15,6 +27,18 @@ const AdminMemberPage = () => {
     suspended: (user) => user.role == "suspended",
   };
 
+  /*
+  useEffect(() => {
+    dispatch(getAllProfilesAPI(1)).then((res) => {
+      console.log(res);
+      setUsers(res.data);
+    });
+  }, []);
+    useEffect(() => {
+      dispatch(getAllProfilesAPI(1));
+      setUsers(users);
+    });
+    */
   useEffect(() => {
     login("admin", "admin").then((data) => {
       if (data.ok === 0) {
@@ -25,8 +49,6 @@ const AdminMemberPage = () => {
       });
     });
   }, []);
-
-  console.log(users);
 
   function handleRegularFilter() {
     setDisplay("all");
