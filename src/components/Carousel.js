e1ae4cfd0562691e3cb0f2a2b7d352f52afe7a9f
rexrexchian <rexrexchian@gmail.com>
2021-11-01 16:58:20 +0800

    homepage carousel

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,6 +1,37 @@
 import React from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
 import { Div } from "atomize";
 
+const HomepageCarousel = () => {
+  return (
+    <Carousel>
+      <Div
+        bgImg="https://images.unsplash.com/photo-1490818387583-1baba5e638af?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1464&q=80"
+        bgSize="cover"
+        bgPos="left"
+        h="30rem"
+      />
+      <Div
+        bgImg="https://images.unsplash.com/photo-1461354464878-ad92f492a5a0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
+        bgSize="cover"
+        bgPos="left"
+        h="30rem"
+      />
+
+      <Div
+        bgImg="https://images.unsplash.com/photo-1592502712628-c5219bf0bc12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1742&q=80"
+        bgSize="cover"
+        bgPos="left"
+        h="30rem"
+      />
+    </Carousel>
+  );
+};
+
+export default HomepageCarousel;
+
+/*
 const CarouselImgae = () => {
   return (
     <Div
@@ -11,6 +42,7 @@ const CarouselImgae = () => {
     />
   );
 };
+*/
 
 /*
 const CarouselLeftButton = () => {
@@ -44,6 +76,7 @@ const CarouselRightButton = () => {
 };
 */
 
+/*
 const Carousel = () => {
   return (
     <Div>
@@ -51,5 +84,4 @@ const Carousel = () => {
     </Div>
   );
 };
-
-export default Carousel;
+*/
